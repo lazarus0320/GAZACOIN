7216e01ee0ae4e49d1ca3e571a78a01fd88ce648
lazarus0320 <astar5327z@gmail.com>
2022-03-20 16:08:55 +0900

    22-03-20 Demo ver 1.0

--- study.py
+++
@@ -1,3 +0,0 @@
-nums = [1, 2, 3, 4]
-doubles = [num * 2 for num in nums if num % 2 == 1]
-print(doubles)
--- study2.py
+++
@@ -1,5 +0,0 @@
-N = int(input())        # 26
-M = N
-i = 0
-M=str(M)
-print((int(M[0])+int(M[1])) % 10)
